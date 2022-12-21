9a9fb19fd750336e5e0e06450753ba0df05f358c
skrbcr <90087433+skrbcr@users.noreply.github.com>
2022-12-22 00:17:48 +0900

    Linux: modify coc-snippets

--- init.lua
+++ init.lua
@@ -102,26 +102,28 @@ end
  -- Coc-explorer
 vim.api.nvim_set_keymap('n', '<space>e', '<Cmd>CocCommand explorer<CR>', {})
 
-if vim.fn.has('unix') == 1 then
-    vim.cmd [[
+-- Coc-snippets
+vim.cmd [[
 " Use <C-l> for trigger snippet expand.
 imap <C-l> <Plug>(coc-snippets-expand)
 
 " Use <C-j> for select text for visual placeholder of snippet.
 vmap <C-j> <Plug>(coc-snippets-select)
+    ]]
+-- Use <C-j> for jump to next placeholder, it's default of coc.nvim
+vim.g.coc_snippet_next = '<C-j>'
 
-" Use <C-j> for jump to next placeholder, it's default of coc.nvim
-let g:coc_snippet_next = '<c-j>'
+-- Use <C-k> for jump to previous placeholder, it's default of coc.nvim
+vim.g.coc_snippet_prev = '<C-k>'
 
-" Use <C-k> for jump to previous placeholder, it's default of coc.nvim
-let g:coc_snippet_prev = '<c-k>'
-
+vim.cmd [[
 " Use <C-j> for both expand and jump (make expand higher priority.)
 imap <C-j> <Plug>(coc-snippets-expand-jump)
 
 " Use <leader>x for convert visual selected code to snippet
 xmap <leader>x  <Plug>(coc-convert-snippet)
-    ]]
+]]
+if vim.fn.has('unix') == 1 then
 end
 
 if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
