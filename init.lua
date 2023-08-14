8bf50b1df31dd06f7934c51bda89b5b3b003b4f1
skrbcr <90087433+skrbcr@users.noreply.github.com>
2023-08-14 10:42:49 +0900

    Linux: fix problem of clipboard

--- init.lua
+++ init.lua
@@ -57,12 +57,12 @@ if vim.fn.has('wsl') == 1 then
     vim.g.clipboard = {
         name = 'win32yank_wsl',
         copy = {
-            ['+'] = 'win32yank.exe -i',
-            ['*'] = 'win32yank.exe -i',
+            ['+'] = 'win32yank.exe -i --crlf',
+            ['*'] = 'win32yank.exe -i --crlf',
         },
         paste = {
-            ['+'] = 'win32yank.exe -o',
-            ['*'] = 'win32yank.exe -o',
+            ['+'] = 'win32yank.exe -o --lf',
+            ['*'] = 'win32yank.exe -o --lf',
         },
         cache_enabled = true,
     }
