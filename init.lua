f5ead6ca3c10c622d3c092c0de0d5bf1c2faaf3b
skrbcr <90087433+skrbcr@users.noreply.github.com>
2023-11-09 22:43:57 +0900

    Linux: Update python_prog

--- init.lua
+++ init.lua
@@ -59,10 +59,10 @@ end
 
 -- python, ruby, perl
 if vim.fn.has('wsl') == 1 then
-    vim.g.python3_host_prog = 'python3.11'
+    vim.g.python3_host_prog = 'python'
 end
 if vim.fn.has('linux') == 1 then
-    vim.g.python3_host_prog = 'python3.11'
+    vim.g.python3_host_prog = 'python'
 end
 if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
     vim.g.python3_host_prog ="python.exe"
