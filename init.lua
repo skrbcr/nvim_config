3283d1b9eacf66083cda1a5ad122aea4ff14f145
skrbcr <90087433+skrbcr@users.noreply.github.com>
2024-08-24 08:20:53 +0900

    Add github copilot

--- init.lua
+++ init.lua
@@ -136,6 +136,7 @@ local plugins = {
 if (vim.fn.has('wsl') == 1) then
     table.insert(plugins, {
         'lervag/vimtex',
+        'github/copilot.vim',
     })
 end
 if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
